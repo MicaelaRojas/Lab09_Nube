808c584c386475f35515013a69f741a3fcdd15c3
unknown <micaela.rojas@tecsup.edu.pe>
2022-05-15 23:15:28 -0500

    Upload

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\FileUploadController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::resource('/productos', ProductoController::class);
+    Route::get('/dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/upload', [FileUploadController::class, 'showUploadForm']);
+Route::post('/upload', [FileUploadController::class, 'storeUploads']);
